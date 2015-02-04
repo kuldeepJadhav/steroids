4cc1d51a48de657dbd07752b28db8f787539da0d
Matti Paksula <matti.paksula@iki.fi>
2015-02-04 11:50:28 +0200

    removes unnecessary if exists check

--- bin/test.cmd
+++ bin/test.cmd
@@ -2,7 +2,6 @@ echo -- Steroids Windows Testrun --
 echo without specs, because those wont run (0 specs, 0 failures)
 echo wat.
 
-if exist "__testApp" rd /s /q "__testApp"
 steroids create __testApp --type=mpa --language=coffee
 cd __testApp
 
